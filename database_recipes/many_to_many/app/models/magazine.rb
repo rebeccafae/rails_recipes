fe2bc90bee2fee6b3db4d9b19cb50f9834f9d6ed
Rebecca Jackson <rjackson@comverge.com>
2015-10-20 12:48:44 -0600

    Recipe 1: Many-to-Many Relationships

--- database_recipes/many_to_many/app/models/magazine.rb
+++ database_recipes/many_to_many/app/models/magazine.rb
@@ -1,2 +1,8 @@
 class Magazine < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+  has_many :semiannual_subscribers,
+            -> {where 'subscriptions.length_in_issues = 6'},
+           through: :subscriptions,
+           source: :reader
 end
--- database_recipes/many_to_many/app/models/reader.rb
+++ database_recipes/many_to_many/app/models/reader.rb
@@ -1,2 +1,4 @@
 class Reader < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
 end
